2c4e5c5c82209ecfcce045f9a35349634de24e22
divashka <66060704+divashka@users.noreply.github.com>
2021-10-31 16:54:24 +0300

    усечение строки

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,10 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength) {
+    return str.slice(0, maxlength - 1) + '…';
+  }
+  return str;
 }
+
+alert(truncate('Вот, что мне хотелось бы сказать на эту тему:', 20));
+alert(truncate('Всем привет!', 20));
+
