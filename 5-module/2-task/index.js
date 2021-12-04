12d0d3058da29f8a91aa1a435409a4b9ec845b32
Diana Kuzmina <dianakuzmina325@gmail.com>
2021-12-04 17:46:16 +0300

    Спрятать/показать текст

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let value = 0;
+  document.querySelector('.toggle-text-button').addEventListener('click', () => {
+    if (value == 0) {
+      document.querySelector('#text').hidden = true;
+      value = 1;
+    } else {
+      document.querySelector('#text').hidden = false;
+      value = 0;
+    }
+  })
 }
